a47ebaaa75443d8baadab1fd9e37da2c1e38bf3b
Leonardo Rocha <83188368+LeoRochaCarmo@users.noreply.github.com>
2024-08-27 06:53:45 -0300

    Delete 2.executando_consultas_SQL directory

--- 2.executando_consultas_SQL/11.funcoes_conversao.sql
+++
@@ -1,5 +0,0 @@
--- Ver o faturamento bruto médio como um texto (CAST)
-SELECT
-    ('O faturamento bruto médio foi ' || 
-    CAST(ROUND(AVG(faturamento_bruto), 2) AS TEXT)) AS faturamento_bruto
-FROM faturamento
